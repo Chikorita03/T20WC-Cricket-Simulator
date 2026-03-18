7de30c7b871f1f92d71f194da35ad06bbab2f1e3
Muskan <brijesh@Brijeshs-MacBook-Air.local>
2026-03-18 22:37:52 +0530

    Synchronized bowler and batsman & created the full ball lifecycle

--- thread_pool/player_threads.cpp
+++ thread_pool/player_threads.cpp
@@ -10,17 +10,23 @@ bool match_running = true;
 void* bowler_thread(void* arg) {
     while (match_running) {
 
-        pthread_mutex_lock(&pitch_mutex);//entering the pitch
+        pthread_mutex_lock(&pitch_mutex); // entering pitch
 
         cout << "[Bowler] Delivering ball" << endl;
         sleep(1);
 
-        cout << "[Batsman] Playing shot" << endl;
-        sleep(1);
+        ball_ready = true;
+        stroke_done = false;
+
+        pthread_cond_signal(&ball_delivered);
+
+        while (!stroke_done) {
+            pthread_cond_wait(&stroke_finished, &pitch_mutex);
+        }
 
         cout << "[Pitch] Ball completed\n" << endl;
 
-        pthread_mutex_unlock(&pitch_mutex); //exiting the pitch
+        pthread_mutex_unlock(&pitch_mutex); // exiting pitch
 
         sleep(1);
     }
@@ -31,8 +37,21 @@ void* batsman_thread(void* arg) {
     int id = *(int*)arg;
 
     while (match_running) {
+
+        pthread_mutex_lock(&pitch_mutex);
+
+        while (!ball_ready) {
+            pthread_cond_wait(&ball_delivered, &pitch_mutex);
+        }
+
         cout << "[Batsman " << id << "] Playing shot" << endl;
         sleep(1);
+
+        stroke_done = true;
+        ball_ready = false;
+
+        pthread_cond_signal(&stroke_finished);
+        pthread_mutex_unlock(&pitch_mutex);
     }
     return NULL;
 }
